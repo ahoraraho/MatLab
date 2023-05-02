80e56ebba2b389e581a33075a0e7d97a4a846d69
alanatilio <alanarapa15@gmail.com>
2023-05-02 10:04:26 -0500

    ejecicios matlab

--- e11_millonario_ecuaciones.m
+++ e11_millonario_ecuaciones.m
@@ -9,7 +9,7 @@
 patrimonio =  input('Ingrese el patrimonio total a repartir:  ');
 
  % Calcula la fortuna total del millonario
-legado_camilo = (1/3) * patrimonio;
+legado_camilo = (1/3) * patrimonio; % Calcula el legado que se le dejó a Camilo
 legado_jorge = (4/3) * legado_camilo; % Calcula el legado que se le dejó a Jorge
 legado_martha = (1/2) * legado_jorge; % Calcula el legado que se le dejó a Martha
 fprintf('A Camilo le corresponde: $%.2f\n', legado_camilo);
--- e7_porcentaje_alumnos.m
+++ e7_porcentaje_alumnos.m
@@ -1,10 +1,9 @@
 %Se quiere determinar el porcentaje de alumnos aprobados, desaprobados y ausentes que hubo en un examen.
-resultados = [15, 17, 16, 19, 15, 11, 18, 09, 20]
+resultados = [15, 17, 16, 19, NaN, NaN, 11, 18, 09, 20, 05];
 
 num_aprobados = sum(resultados >= 13 & ~isnan(resultados));
 num_desaprobados = sum(resultados < 13 & ~isnan(resultados));
 num_ausentes = sum(isnan(resultados));
-
 
 porcentaje_aprobados = num_aprobados / (num_aprobados + num_desaprobados + num_ausentes) * 100;
 porcentaje_desaprobados = num_desaprobados / (num_aprobados + num_desaprobados + num_ausentes) * 100;
