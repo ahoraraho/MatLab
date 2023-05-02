dbd9bfabfda3f2e853a9ba4b2305267d6f48c40b
Araho group <arahogrou@gmail.com>
2023-05-02 09:27:15 -0500

    ejercicios corregidos y funcionales

--- e9.m
+++
@@ -1,4 +0,0 @@
-% A una reunión asistieron n personas, ¿cuántos apretones de mano hubo?
-num_personas = input('Ingrese el numero der personas que asistieron a la fiesta:  ');
-num_apretones = num_personas * (num_personas - 1)/2;
-fprintf('En la reunion hubo %d apretones de manon: %f\n', num_apretones);
